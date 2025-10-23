4313229651fefd032c7284ce08ed13b7bb06e106
MahirFaisal <mahirtayeb1@gmail.com>
2025-10-23 17:20:20 +0600

    leetcode 1527 solve

---
+++ SQL/patients_with_a_condition_1527.sql
@@ -0,0 +1,12 @@
+select 
+    patient_id,
+    patient_name,
+    conditions
+from patients
+where conditions like '%DIAB1%'
+group by 1, 2, 3
+having conditions not like 'SADIAB100' 
+    and conditions not like '%+DIAB100';
+
+
+-- where using (and , and) makes it slow but this code is faster 
