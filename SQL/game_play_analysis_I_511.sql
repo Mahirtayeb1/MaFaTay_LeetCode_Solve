9d9cc1e4dd57f7ba270ca2ac242ed5d0e458eb02
MahirFaisal <mahirtayeb1@gmail.com>
2025-11-20 09:07:05 +0600

    new ans

--- SQL/game_play_analysis_I_511.sql
+++ SQL/game_play_analysis_I_511.sql
@@ -1,11 +1,16 @@
--- most less runtime
-select player_id,min(event_date)as first_login from Activity
-group by player_id
 
--- my solve:
+
+-- my solve 1:
 select
     player_id,
     min(event_date) as first_login
 from Activity
 group by 1
 order by 1 asc, 2 asc;
+
+-- my solve 2:
+select 
+    player_id,
+    min(event_date) as first_login
+from Activity
+group by 1;
